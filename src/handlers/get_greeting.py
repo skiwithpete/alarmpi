868e6ab0eeabddd597bbcd10ddb1c6536a6d95d5
Lauri Ajanki <laareni@yahoo.co.uk>
2020-04-22 22:54:42 +0300

    Fix time of day detection bug. Better greeting formatting.

--- src/handlers/get_greeting.py
+++ src/handlers/get_greeting.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 import time
 import datetime
+import calendar
+
 import num2words
 
 from src import apcontent
@@ -15,25 +17,26 @@ class Greeting(apcontent.AlarmpiContent):
 
     def build(self):
         today = datetime.datetime.today()
-        day_of_month = num2words.num2words(today.day, ordinal=True)
 
-        # current date and time as a spoken sentance,
-        # eg. Wednesday August Twenty Second, 22:37
-        current_date = "{}{}".format(time.strftime("%A %B "), day_of_month)
+        current_weekday = calendar.day_name[today.weekday()]
+        current_month = calendar.month_name[today.month]
+        day_of_month = num2words.num2words(today.day, ordinal=True)
         current_time = time.strftime("%I:%M %p")  # eg. 6:36 pm
 
         if today.hour < 12:
             period = 'morning'
-        elif today.hour >= 12:
-            period = 'afternoon'
         elif today.hour >= 17:
             period = 'evening'
+        elif today.hour >= 12:
+            period = 'afternoon'
 
-        greeting = "Good {}, {}. It's {}. The time is {}.\n\n".format(
-            period,
-            self.section_data["name"],
-            current_date,
-            current_time
+        greeting = "Good {period}, {name}. It's {weekday} {month} {day_of_month}. The time is {time}.\n\n".format(
+            period=period,
+            name=self.section_data["name"],
+            weekday=current_weekday,
+            month=current_month,
+            day_of_month=day_of_month,
+            time=current_time
         )
 
         self.content = greeting
