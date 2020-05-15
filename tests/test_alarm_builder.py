dc46855e97fa3d9ff1f4f5941be06d717242dae8
Lauri Ajanki <laareni@yahoo.co.uk>
2020-05-15 22:07:36 +0300

    Use test config file.

--- tests/test_alarm_builder.py
+++ tests/test_alarm_builder.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os.path
 import unittest
 from unittest import TestCase
 from unittest.mock import patch
@@ -20,30 +21,29 @@ class AlarmProcessingTestCase(TestCase):
     to the configuration file?
     """
 
-    def setUp(self):
-        """Create an Alarm object using a dummy AlarmEnv."""
-        env = alarmenv.AlarmEnv("mock_config_file")
+    @patch("src.alarmenv.AlarmEnv.get_config_file_path")
+    def setUp(self, mock_get_config_file_path):
+        """Create an AlarmEnv using test_alarm.conf."""
+        mock_get_config_file_path.return_value = os.path.join(os.path.dirname(__file__), "test_alarm.conf")
+        env = alarmenv.AlarmEnv("dummy_config_name") # name will be ignored, above path will be used instead
+        env.setup() # parse the configuration (also validates it, no need to mock)
         self.alarm = alarm_builder.Alarm(env)
-        self.alarm.env.radio_url = False
 
-    @patch("src.alarmenv.AlarmEnv.get_value")
-    @patch("src.alarm_builder.Alarm.get_content_parser_class")
-    @patch("src.alarmenv.AlarmEnv.get_enabled_sections")
-    def test_first_enabled_tts_client_chosen(self, mock_get_enabled_sections, mock_get_content_parser_class, mock_get_value):
+    def test_first_enabled_tts_client_chosen(self):
         """Does get_tts_client choose the first enabled client?"""
-        mock_get_enabled_sections.return_value = ["google_translate_tts"]
-
-        mock_get_content_parser_class.return_value = get_google_translate_tts.GoogleTranslateTTSManager
-        mock_get_value.return_value = None
+        self.alarm.env.config.set("google_gcp_tts", "enabled", "0")
+        self.alarm.env.config.set("google_translate_tts", "enabled", "1")
+        self.alarm.env.config.set("festival_tts", "enabled", "0")
 
         client = self.alarm.get_tts_client()
         self.assertIsInstance(
             client, get_google_translate_tts.GoogleTranslateTTSManager)
 
-    @patch("src.alarmenv.AlarmEnv.get_enabled_sections")
-    def test_default_TTS_client_chosen_when_none_set(self, mock_get_enabled_sections):
+    def test_default_TTS_client_chosen_when_none_set(self):
         """Is the Festival client chosen in get_tts_client when none is explicitly enaled?"""
-        mock_get_enabled_sections.return_value = None
+        self.alarm.env.config.set("google_gcp_tts", "enabled", "0")
+        self.alarm.env.config.set("google_translate_tts", "enabled", "0")
+        self.alarm.env.config.set("festival_tts", "enabled", "0")
 
         client = self.alarm.get_tts_client()
         self.assertIsInstance(client, get_festival_tts.FestivalTTSManager)
@@ -66,48 +66,27 @@ class AlarmProcessingTestCase(TestCase):
 
         # run get_content_parser_class for each handler name and compare to the corresponding response_class
         for response_class in response_classes:
-            created_class = self.alarm.get_content_parser_class("")
+            created_class = self.alarm.get_content_parser_class("dummy_section")
             self.assertIs(created_class, response_class)
 
+    @patch("src.alarmenv.AlarmEnv._testnet")
     @patch("src.alarm_builder.Alarm.play_beep")
-    @patch("src.alarm_builder.Alarm.gui_running")
-    @patch("src.alarmenv.AlarmEnv.config_has_match")
-    def test_beep_played_when_no_network(self, mock_config_has_match, mock_gui_running, mock_play_beep):
+    def test_beep_played_when_no_network(self, mock_play_beep, mock_testnet):
         """Is the beep played when no network connection is detected?"""
-        mock_gui_running.return_value = False
-        mock_config_has_match.return_value = True  # is GUI running
-        self.alarm.env.netup = False
-        self.alarm.build_and_play()
+        mock_testnet.return_value = False
 
+        self.alarm.play("dummy_content")
         mock_play_beep.assert_called()
 
+
+    @patch("src.alarmenv.AlarmEnv.config_has_match")
     @patch("src.alarm_builder.Alarm.play_beep")
-    @patch("src.alarm_builder.Alarm.gui_running")
-    @patch("src.alarmenv.AlarmEnv.config_has_match")
-    def test_beep_played_when_no_readaloud(self, mock_config_has_match, mock_gui_running, mock_play_beep):
+    def test_beep_played_when_no_readaloud(self, mock_play_beep, mock_config_has_match):
         """Is the beep played when readaloud = 0 is set in the configuration?"""
-        mock_gui_running.return_value = False
-        mock_config_has_match.return_value = False  # is GUI running
-        self.alarm.env.netup = True
-        self.alarm.build_and_play()
+        mock_config_has_match.return_value = False  # mock TTS detection call
 
+        self.alarm.play("dummy_content")
         mock_play_beep.assert_called()
-
-    @patch("src.alarm_builder.Alarm.play_radio")
-    @patch("src.alarm_builder.Alarm.get_tts_client")
-    @patch("src.alarm_builder.Alarm.generate_content")
-    @patch("src.alarm_builder.Alarm.gui_running")
-    @patch("src.alarmenv.AlarmEnv.config_has_match")
-    def test_radio_played_when_enabled(self, mock_config_has_match, mock_gui_running, mock_generate_content, mock_get_tts_client, mock_play_radio):
-        """Is a radio stream opened when radio is enabled in the config?"""
-        mock_gui_running.return_value = False
-        mock_config_has_match.return_value = True  # is GUI running
-
-        self.alarm.env.netup = True
-        self.alarm.env.radio_url = True
-
-        self.alarm.build_and_play()
-        mock_play_radio.assert_called()
 
 
 if __name__ == "__main__":
