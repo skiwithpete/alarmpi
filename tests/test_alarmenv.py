0045068f59d493616af5a9833dcdbb521b99d72b
Lauri Ajanki <laareni@yahoo.co.uk>
2020-05-15 23:06:18 +0300

    Use test config file.

--- tests/test_alarmenv.py
+++ tests/test_alarmenv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import os.path
 import configparser
 import unittest
 from unittest import TestCase
@@ -14,61 +15,54 @@ class AlarmEnvTestCase(TestCase):
     correctly?
     """
 
-    def setUp(self):
-        self.env = alarmenv.AlarmEnv("")
+    @patch("src.alarmenv.AlarmEnv.get_config_file_path")
+    def setUp(self, mock_get_config_file_path):
+        mock_get_config_file_path.return_value = os.path.join(os.path.dirname(__file__), "test_alarm.conf")
+        self.env = alarmenv.AlarmEnv("dummy_config_name")
+        self.env.setup()
 
     @patch("src.alarmenv.AlarmEnv.get_sections")
     def test_validate_config_raises_runtime_error_on_invalid(self, mock_get_sections):
         """Does validate_config raise RuntimeError when configparser detects invalid
         options?
         """
-        mock_get_sections.side_effect = configparser.NoSectionError("foo")
+        mock_get_sections.side_effect = configparser.NoSectionError("")
         self.assertRaises(RuntimeError, self.env.validate_config)
 
-    def test_invalid_config_file_raises_error(self):
+    @patch("src.alarmenv.AlarmEnv.get_config_file_path")
+    def test_invalid_config_file_raises_error(self, mock_get_config_file_path):
         """Does trying to create an AlarmEnv with a non-existing configuration file
         raise RuntimeError?
         """
-        env = alarmenv.AlarmEnv("foo.config")  # create a new AlarmEnv with non-existing file
+        # Create a new AlarmEnv with a non-existing parameter but point to the existing configuration
+        # to avoid FileNotFoundError in get_config_file_path.
+        mock_get_config_file_path.return_value = os.path.join(os.path.dirname(__file__), "test_alarm.conf")
+        env = alarmenv.AlarmEnv("dummy.conf")
+        env.config_file = "dummy.conf"  # config_file attribute is set to existing test file, manually change back to dummy file before calling setup
         self.assertRaises(RuntimeError, env.setup)
 
-    @patch("src.alarmenv.AlarmEnv.get_value")
-    def test_no_match_on_invalid_config_section(self, mock_get_value):
-        """Does config_has_match return False on invalid section name?"""
-        mock_get_value.return_value = "mock_response"
-
-        match = self.env.config_has_match("nosuchsection", "type", "value")
+    def test_no_match_on_invalid_config_section(self):
+        """Does config_has_match return False (instead of an Exception) when checking for invalid section?"""
+        match = self.env.config_has_match("invalidsection", "enabled", "1")
         self.assertFalse(match)
 
-    @patch("src.alarmenv.AlarmEnv.get_value")
-    def test_match_on_valid_config_keys(self, mock_get_value):
+    def test_match_on_valid_config_keys(self):
         """Does config_has_match return True on valid section, option, key combination?"""
-        mock_get_value.return_value = "value"
-
-        match = self.env.config_has_match("section", "option", "value")
+        match = self.env.config_has_match("BBC_news", "enabled", "1")
         self.assertTrue(match)
 
-    @patch("configparser.ConfigParser.sections")
-    def test_get_sections_without_main(self, mock_sections):
-        """Does get_sections return section names without the main section?"""
-        mock_sections.return_value = ["main", "greeting", "openweathermap", "BBC_news", "google_gcp_tts",
-                                      "google_translate_tts", "festival_tts", "radio"]
-        read_sections = self.env.get_sections(["main"])
+    def test_get_sections_with_exclusions(self):
+        """Does get_sections return correct section names when called with exclusions?"""
+        read_sections = self.env.get_sections(excludes=["main", "greeting"])
 
-        filtered_sections = ["greeting", "openweathermap", "BBC_news", "google_gcp_tts",
+        filtered_sections = ["alarm", "polling", "openweathermap", "BBC_news", "google_gcp_tts",
                              "google_translate_tts", "festival_tts", "radio"]
-        self.assertEqual(read_sections, filtered_sections)
+        self.assertCountEqual(read_sections, filtered_sections)  # note: checks for same elements as well counts
 
-    @patch("src.alarmenv.AlarmEnv.config_has_match")
-    @patch("src.alarmenv.AlarmEnv.get_sections")
-    def test_read_content_sections(self, mock_get_sections, mock_config_has_match):
-        """Does get_enabled_type_sections return correct content section names?"""
-        mock_get_sections.return_value = ["greeting", "openweathermap", "BBC_news"]
-        # sets the first 2 items as 'enabled'
-        mock_config_has_match.side_effect = [True, True, True, True, False, False]
-
+    def test_read_content_sections(self):
+        """Does get_enabled_sections return correct content section names?"""
         sections = self.env.get_enabled_sections("content")
-        self.assertEqual(sections, ["greeting", "openweathermap"])
+        self.assertEqual(sections, ["BBC_news"])
 
 
 if __name__ == "__main__":
