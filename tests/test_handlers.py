9775bceb67b54f40f456d0c5e733ea9d91dd761b
Lauri Ajanki <lauri.ajanki@gmail.com>
2021-07-26 18:51:25 +0300

    Update remaining tests

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,7 +1,7 @@
 import requests
-from unittest.mock import patch
+from unittest.mock import patch, mock_open
 
-from src.handlers import get_open_weather, get_next_trains
+from src.handlers import get_weather, get_next_trains
 
 
 
@@ -14,13 +14,16 @@ def test_failed_train_api_request_returns_error_template(mock_fetch_daily_train_
 
     assert res is None
 
-@patch("src.handlers.get_open_weather.OpenWeatherMapClient.get_weather")
+@patch("src.handlers.get_weather.OpenWeatherMapClient.get_weather")
 def test_failed_weather_api_request_returns_error_template(mock_get_weather):
     """Does fetch_and_format_weather return None if API call fails"""
     mock_get_weather.side_effect = requests.exceptions.RequestException()
 
     section_data = {"credentials": None, "city_id": None, "units": None}
-    parser = get_open_weather.OpenWeatherMapClient(section_data)
-    res = parser.fetch_and_format_weather()
 
-    assert res is None
+    # Mock opening non existing credentials file
+    with patch("builtins.open", mock_open(read_data="data")) as mock_file:
+        parser = get_weather.OpenWeatherMapClient(section_data)
+        res = parser.fetch_and_format_weather()
+
+        assert res is None
