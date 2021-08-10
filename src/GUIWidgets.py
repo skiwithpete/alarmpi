82602b258ae278da19dcd91cdb57edae40885ecc
Lauri Ajanki <lauri.ajanki@gmail.com>
2021-08-10 20:42:32 +0300

    Drop title label

--- src/GUIWidgets.py
+++ src/GUIWidgets.py
@@ -202,7 +202,7 @@ class SettingsWindow(QWidget):
 
         # ** Bottom level main buttons **
         control_button_config = [
-            ButtonConfig(text="Play now", position=(0, 0), icon="play64x64.png"),
+            ButtonConfig(text="Play Now", position=(0, 0), icon="play64x64.png"),
             ButtonConfig(text="Toggle\nWindow", position=(0, 1), icon="window64x64.png"),
             ButtonConfig(text="Toggle\nBrightness", position=(0, 2), icon="brightness64x64.png"),
             ButtonConfig(text="Close", position=(0, 3), slot=self.clear_labels_and_close)
@@ -223,7 +223,6 @@ class SettingsWindow(QWidget):
             bottom_grid.addWidget(button, *config.position)
 
         # ** Left grid: misc settings **
-        settings_title = QLabel("Alarm settings", self)
         self.readaloud_checkbox = QCheckBox("Enable Text-to-Speech alarm", self)
         self.nightmode_checkbox = QCheckBox("Enable Nightmode", self)
         self.alarm_brightness_checkbox = QCheckBox("Full Brightness on alarm", self)
@@ -239,10 +238,9 @@ class SettingsWindow(QWidget):
         self.alarm_time_error_label = QLabel(self)
         self.alarm_time_error_label.setStyleSheet("color: #FF1414;")
 
-        left_grid.addWidget(settings_title, 0, 0)
-        left_grid.addWidget(self.readaloud_checkbox, 1, 0)
-        left_grid.addWidget(self.nightmode_checkbox, 2, 0)
-        left_grid.addWidget(self.alarm_brightness_checkbox, 3, 0)
+        left_grid.addWidget(self.readaloud_checkbox, 0, 0)
+        left_grid.addWidget(self.nightmode_checkbox, 1, 0)
+        left_grid.addWidget(self.alarm_brightness_checkbox, 2, 0)
 
         volume_grid = QGridLayout()
         self.volume_slider = QSlider(Qt.Horizontal, self)
@@ -252,13 +250,13 @@ class SettingsWindow(QWidget):
 
         volume_grid.addWidget(self.volume_slider, 0, 0)
         volume_grid.addWidget(self.volume_label, 0, 1)
-        left_grid.addLayout(volume_grid, 4, 0)
+        left_grid.addLayout(volume_grid, 3, 0)
         self.volume_slider.setMaximumWidth(180)
         volume_grid.setHorizontalSpacing(20)
 
-        left_grid.addWidget(self.alarm_time_status_label, 5, 0)
-        left_grid.addWidget(self.alarm_time_error_label, 6, 0)
-        left_grid.addWidget(self.radio_station_combo_box, 7, 0)
+        left_grid.addWidget(self.alarm_time_status_label, 4, 0)
+        left_grid.addWidget(self.alarm_time_error_label, 5, 0)
+        left_grid.addWidget(self.radio_station_combo_box, 6, 0)
 
         # Add grids to base layout
         base_layout.addLayout(left_grid, 0, 0)
