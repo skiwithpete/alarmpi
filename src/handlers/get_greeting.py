141fc93f6b7bc290c77e2a8658fbcea18d4d0a5a
Lauri Ajanki <lauri.ajanki@gmail.com>
2021-10-07 22:25:36 +0300

    Force locale, read alarm time override

--- src/handlers/get_greeting.py
+++ src/handlers/get_greeting.py
@@ -1,35 +1,39 @@
-import time
-import datetime
-import calendar
+from datetime import datetime
+import locale
 
 import num2words
 
 from src import apcontent
 
 
+# Force the use of C locale for strftime calls
+locale.setlocale(locale.LC_TIME, "C")
+
 class Greeting(apcontent.AlarmpiContent):
     """Creates greeting messages based on current time of day."""
 
-    def __init__(self, section_data):
+    def __init__(self, section_data, alarm_time_override):
         super().__init__(section_data)
+        self.alarm_time_override = alarm_time_override
 
     def build(self):
-        today = datetime.datetime.today()
-
-        # Use the 'C' locale for generating weekday and month names
-        with calendar.different_locale("C"):
-            current_weekday = calendar.day_name[today.weekday()]
-            current_month = calendar.month_name[today.month]
-
+        today = datetime.today()
         day_of_month = num2words.num2words(today.day, ordinal=True)
-        current_time = time.strftime("%I:%M %p")  # eg. 6:36 pm
+        current_weekday = today.strftime("%A")
+        current_month = today.strftime("%B")
+
+        # Convert 24 hour override format to 12 hour format
+        if self.alarm_time_override:
+            alarm_time = datetime.strptime(self.alarm_time_override, "%H:%M").strftime("%I:%M %p") # eg. 6:36 pm
+        else:
+            alarm_time = datetime.now().strftime("%I:%M %p")
 
         if today.hour < 12:
-            period = 'morning'
+            period = "morning"
         elif today.hour >= 17:
-            period = 'evening'
+            period = "evening"
         elif today.hour >= 12:
-            period = 'afternoon'
+            period = "afternoon"
 
         greeting = "Good {period}, {name}. It's {weekday} {month} {day_of_month}. The time is {time}.\n\n".format(
             period=period,
@@ -37,7 +41,7 @@ class Greeting(apcontent.AlarmpiContent):
             weekday=current_weekday,
             month=current_month,
             day_of_month=day_of_month,
-            time=current_time
+            time=alarm_time
         )
 
         self.content = greeting
